67f52c66791bab4714c4f1e77b2729ffb04c230f
grmnkr211 <140467110+grmnkr211@users.noreply.github.com>
2023-08-12 23:57:30 -0700

    Update App.java

--- App.java
+++ App.java
@@ -1,60 +1,32 @@
+//German 
+//Updated 8/12/2024
 
-//From Video
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-
-//Extras
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-
+ 
 public class App extends Application {
-
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Cluck&Co.fxml"));
-        primaryStage.setTitle("Cluck & Co.");
-        primaryStage.setScene(new Scene(root, 1315, 890));
-        primaryStage.show();
-    }
-
-
-    //Ignore: 
-    /* 
+    
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World' ");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+            primaryStage.setTitle("Cluck & Co.");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            }  
+    }
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Cluck & Co.");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-*/
-    
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         launch(args);
     }
 }
