18044bd3642619ffe6241792d9b40ff1ecb6fa87
DavReggae <54373665+DavReggae@users.noreply.github.com>
2023-08-15 15:34:09 -0700

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,19 +1,23 @@
-public enum Product {
-    SHIRT1("shirt1.jpg",25f), SHIRT2("shirt2.jpg",20f), HAT("hat.jpg",20f), HOODIE("hoodie.jpg",50f)
+package CluckCo.shoppingCart.home;
 
+public enum Product {
+    
+    APPLE("Apple.jpg", 0.55f), MILK("Milk.jpg", 0.78f), 
+    JUICE("Juice.jpg", 0.56f), LETTUCE("Lettuce.jpg", 0.56f);
+    //price and image name *EDIT LATER FOR CLUCK&CO products*
     private String imageFile;
     private float price;
 
-    Product(String imageFile,float price){
+    private Product(String imageFile, float price){
         this.imageFile = imageFile;
-        this.price = price;
+        this.price = price;   
     }
 
-    public String getImageFile() {
+    public String getImageFile(){
         return imageFile;
     }
 
-    public float getPrice() {
-        return price;
+    public float getPrice(){
+        return this.price;
     }
 }
