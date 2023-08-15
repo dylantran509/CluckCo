0ecdfc4241b4280f2cb22763a44e39c17e29e41b
dylantran509 <140466822+dylantran509@users.noreply.github.com>
2023-08-15 16:48:12 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.FXML.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Product Search");
+    }
+}
