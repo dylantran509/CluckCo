e68a65e90e877a0df253ad92821eca0711dd95d6
dylantran509 <140466822+dylantran509@users.noreply.github.com>
2023-08-15 22:14:34 -0700

    Add files via upload

---
+++ SearchMain.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.FXML.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// Class name: SearchMain
+// Date of code: 8-11-23
+// Dylan Tran
+// This class runs the search function in order to open the window that the user will be able to utilize the search feature from. The functionality is handled
+// by the SearchController class.
+// There are no data structures in this class.
+// There are no algorithms in this class.
+
+public class SearchMain extends Application {
+
+    // This method runs the search feature to open the search window on the home page of the e-commerce store.
+    // parameters: String[] args
+    // return: nothing but prints "Search" as a label for the search window
+    public static void main(String[] args) throws Exception {
+        System.out.println("Search");
+    }
+}
