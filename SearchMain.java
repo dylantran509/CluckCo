44cbfaed636dd08c7e57062346d0a05efbd64136
dylantran509 <140466822+dylantran509@users.noreply.github.com>
2023-08-16 16:11:46 -0700

    Add files via upload

--- SearchMain.java
+++ SearchMain.java
@@ -4,20 +4,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// Class name: SearchMain
-// Date of code: 8-11-23
-// Dylan Tran
-// This class runs the search function in order to open the window that the user will be able to utilize the search feature from. The functionality is handled
-// by the SearchController class.
-// There are no data structures in this class.
-// There are no algorithms in this class.
+/**
+ * a.) Class name: SearchMain
+ * b.) Date of code: 8-11-23
+ * c.) Dylan Tran
+ * d.) This class runs the search function in order to open the window that the user will be able to utilize the search feature from. The functionality is handled
+ * by the SearchController class.
+ * e.) There are no data structures in this class.
+ * f.) There are no algorithms in this class.
+*/
 
-public class SearchMain extends Application {
+public class SearchMain extends Application
+{
+    /**
+     * This method runs the search feature to open the search window on the home page of the e-commerce store and prints the word "Search" on the interface.
+     * @param String[] args String class with Java command line arguments for running main method
+     */
 
-    // This method runs the search feature to open the search window on the home page of the e-commerce store.
-    // parameters: String[] args
-    // return: nothing but prints "Search" as a label for the search window
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         System.out.println("Search");
     }
 }
