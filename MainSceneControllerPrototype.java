02c8d01d5eb0d30c2739c4d6fdca0cd5be4ad9d5
dylantran509 <140466822+dylantran509@users.noreply.github.com>
2023-08-16 16:13:04 -0700

    Add files via upload

---
+++ MainSceneControllerPrototype.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * a.) Class name: MainSceneControllerPrototype
+ * b.) Date of code: 7-20-23
+ * c.) Dylan Tran
+ * d.) This class controls a prototype for the main scene which displays a search bar along with a text field that allows users to type in a keyword. This class was
+ * used for testing.
+ * e.) There are no data structures in this class.
+ * f.) There are no algorithms in this class.
+ */
+
+public class MainSceneControllerPrototype
+{
+    // search bar text field for allowing users to enter a key word to search
+    @FXML
+    private TextField SearchBar;
+
+    /**
+     * This method is a search button detecting when a user clicks to search a key word.
+     */
+
+    @FXML
+    void search(ActionEvent event)
+    {
+
+    }
+}
