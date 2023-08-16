3ee77df5b8332b75f48d854b31566a1c21c81633
dylantran509 <140466822+dylantran509@users.noreply.github.com>
2023-08-16 16:12:24 -0700

    Add files via upload

--- ProductPrototype.java
+++ ProductPrototype.java
@@ -1,38 +1,50 @@
-// a.) Class name: Product
-// b.) 7-27-23
-// c.) Dylan Tran
-// d.) This class creates products to be displayed on the home page, which is also the product page.
-// e.) There are no data structures in this class.
-// f.) There are no algorithms in this class.
+/*
+ * a.) Class name: ProductPrototype
+ * b.) 7-27-23
+ * c.) Dylan Tran
+ * d.) This class creates products to be displayed on the home page, which is also the product page.
+ * e.) There are no data structures in this class.
+ * f.) There are no algorithms in this class.
+ */
 
-public enum Product {
-
+public enum ProductPrototype
+{
     // declared objects for products containing images of the products - a shirt, a hoodie, a hat, and a sticker - along with their prices
-    SHIRT("shirt.jpg",25f), STICKER("sticker.jpg",5f), HAT("hat.jpg",20f), HOODIE("hoodie.jpg",50f);
+    SHIRT("shirt.jpg", 25f),
+    STICKER("sticker.jpg", 5f),
+    HAT("hat.jpg", 20f),
+    HOODIE("hoodie.jpg", 50f);
 
     // variable for image files to be able to reference the actual files saved to device
-    private String imageFile;
+    private String image;
 
     // variable for prices to be able to assign numerical prices to the items
     private float price;
 
     // general object for products; these contain the products' respective image files as well as their prices
-    Product(String imageFile,float price){
-        this.imageFile = imageFile;
+    ProductPrototype(String image,float price)
+    {
+        this.image = image;
         this.price = price;
     }
 
-    // This method takes the name of an image file saved to a device and returns it to a product object.
-    // parameters: N/A
-    // return: image file
-    public String getImageFile() {
-        return imageFile;
+    /**
+     * This method takes the name of an image file saved to a device and returns it to a product object.
+     * @return image image of product
+     */
+    
+    public String getImageFile()
+    {
+        return image;
     }
 
-    // This method returns a price for a product.
-    // parameters: N/A
-    // return: price
-    public float getPrice() {
+    /**
+     * This method returns a price for a product.
+     * @return price product price
+     */
+    
+    public float getPrice()
+    {
         return price;
     }
 }
