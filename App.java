5d8589cf632fe7fce61c46c261f972c1623c8343
grmnkr211 <140467110+grmnkr211@users.noreply.github.com>
2023-08-23 23:27:12 -0700

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,20 @@
-//German 
-//Updated 8/12/2024
-//Working Home page.
+/**
+ * Home page for Cluck & Co. E-Commerce site. Site will display items
+ * being sold in our site. It includes functions like search, add to
+ * cart, and view cart. 
+ * @author German Wong, Dylan Tran, David Regio, Simon Chang
+ * @version 1.0
+ * Date:            7/20/2023
+ * Last Updated:    8/15/2015
+ * Data Structure:  ArrayList
+ */
 
 
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,12 +24,12 @@ import javafx.stage.Stage;
  
 public class App extends Application {
     
-    
     @Override
     public void start(Stage primaryStage) {
 
         try{
             Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+
             primaryStage.setTitle("Cluck & Co.");
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
@@ -27,7 +38,17 @@ public class App extends Application {
             }  
     }
 
-    public static void main(String[] args) {
+
+    public static String getUserFilePath(String filePath){
+        Path path = Paths.get(filePath);
+        return path.toAbsolutePath().toString();
+    }
+
+    public static void main(String[] args)throws IOException {
         launch(args);
+        
+        File cart = new File(getUserFilePath("cart.txt")); 
+        cart.delete();
+        cart.createNewFile();
     }
 }
